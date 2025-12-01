cef8e44ac2b05d658e41e78512f0690acacfa9ba
Siddharth <csiddharth380@gmail.com>
2025-12-01 18:24:12 +0530

    Lenghtened the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,24 @@ import Hero from "../components/hero";
 import Desc from "../components/description";
 import Feat from "../components/feat";
 import { initLenis } from "../lib/lenis";
+import { FAQ } from "../components/faq";
+import { HowItWorks } from "../components/working";
+import { Benefits } from "../components/benefits";
 
 export default function Home() {
-
   useEffect(() => {
-    const lenis = initLenis()
-    return () => lenis.destroy()
-  }, [])
-
+    const lenis = initLenis();
+    return () => lenis.destroy();
+  }, []);
 
   return (
     <div className="overflow-hidden bg-black">
       <Hero />
       <Desc />
-      <Feat/>
+      <Feat />
+      <HowItWorks />
+      <Benefits />
+      <FAQ />
     </div>
   );
 }
